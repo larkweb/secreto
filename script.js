ad8ffc550d8b56e8c0624aa0b3c1b7199fa20738
larkweb <148163767+larkweb@users.noreply.github.com>
2024-03-21 14:14:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("redirectButton").addEventListener("click", function() {
+    window.location.href = "index.html"; // Cambia "index.html" por el nombre de la página a la que quieres redirigir
+  });
