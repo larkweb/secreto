62f72f6d797dace1cd71c8adb064848709a01a6c
larkweb <148163767+larkweb@users.noreply.github.com>
2024-03-21 14:17:39 -0500

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("redirectButton").addEventListener("click", function() {
-    window.location.href = "index.html"; // Cambia "index.html" por el nombre de la página a la que quieres redirigir
+    window.location.href = "flores.html"; // Cambia "index.html" por el nombre de la página a la que quieres redirigir
   });
